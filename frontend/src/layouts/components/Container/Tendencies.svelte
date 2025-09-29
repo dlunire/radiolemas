31a6a872b4932c869ebec3ee0a5d1dac1c65984c
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-09-29 07:32:47 -0500

    Update

--- frontend/src/layouts/components/Container/Tendencies.svelte
+++ frontend/src/layouts/components/Container/Tendencies.svelte
@@ -1,15 +1,19 @@
+<script>
+    import TrendItemNews from "./TrendItemNews.svelte";
+
+</script>
 <section class="section section--home section--news" id="tendencies">
-    <div class="section__inner section__inner--news section__inner--padding">
+    <div class="section__inner section__inner--article">
         <h2 class="section__title section__title--news" aria-label="Recientes">
             Noticias en tendencias
         </h2>
 
         <div class="tendencies">
             <section class="trend">
-                <article class="trend__item"></article>
-                <article class="trend__item"></article>
-                <article class="trend__item"></article>
-                <article class="trend__item"></article>
+                <TrendItemNews />
+                <TrendItemNews />
+                <TrendItemNews />
+                <TrendItemNews />
             </section>
         </div>
     </div>
---
+++ frontend/src/layouts/components/Container/TrendItemNews.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let title: string = "Título de la noticia"
+    export let category: string = "Nombre de categoría";
+</script>
+<article class="trend__item">
+    <header class="trend__item-header">Foto</header>
+
+    <div class="trend__item-content">
+        <span class="trend__item-label">{category}</span>
+        <h2 class="trend__item-title" aria-label="{title}">
+            {title}
+        </h2>
+        <div class="trend__item-resume">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quas ducimus. Minima velit...
+        </div>
+    </div>
+</article>
