6cbdb11a4ff0c2bd0f354960516cc13246ed04df
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-10-07 11:13:27 -0500

    Update

--- frontend/src/layouts/components/Header/WindowMenu.svelte
+++ frontend/src/layouts/components/Header/WindowMenu.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     // your script goes here
+    let windows: HTMLElement | null = null;
+
+    onMount(() => {
+        if (!(windows instanceof HTMLElement)) return;
+        document.body.appendChild(windows);
+    });
 </script>
 
-<section class="window-container">
+<section class="window-container" bind:this={windows}>
     
 </section>
