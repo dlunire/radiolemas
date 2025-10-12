c9c4930b73ee523f39ae674dd85e3d264134df37
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-10-12 12:00:22 -0500

    Update

--- frontend/src/layouts/components/Footer/MenuFooter.svelte
+++ frontend/src/layouts/components/Footer/MenuFooter.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
+    import IconContact from "../../icons/IconContact.svelte";
+    import IconHome from "../../icons/IconHome.svelte";
+    import IconLive from "../../icons/IconLive.svelte";
+    import IconNews from "../../icons/IconNews.svelte";
+    import IconStation from "../../icons/IconStation.svelte";
     import Link from "../../routers/components/Link.svelte";
 </script>
 
 <div class="grow">
-    <h3 class="footer__title footer__title--menu" aria-label="Navegación">Navegación</h3>
+    <h3 class="footer__title footer__title--menu" aria-label="Navegación">
+        Navegación
+    </h3>
     <menu class="menu menu--footer">
         <li class="menu__item menu__item--footer">
             <Link
                 href="/#home"
                 native={true}
-                className="menu__link menu__link--footer">Home</Link
+                className="menu__link menu__link--footer"
             >
+                <IconHome />
+                <span>Home</span>
+            </Link>
         </li>
 
         <li class="menu__item menu__item--footer">
             <Link
                 href="/#programming"
                 native={true}
-                className="menu__link menu__link--footer">Programación</Link
+                className="menu__link menu__link--footer">
+                    <IconStation />
+                    <span>Programación</span>
+                </Link
             >
         </li>
 
@@ -25,7 +38,10 @@
             <Link
                 href="/online"
                 native={false}
-                className="menu__link menu__link--footer">En Vivo</Link
+                className="menu__link menu__link--footer">
+                    <IconLive />
+                    <span>En Vivo</span>
+                </Link
             >
         </li>
 
@@ -33,14 +49,20 @@
             <Link
                 href="/news"
                 native={false}
-                className="menu__link menu__link--footer">Noticias</Link
+                className="menu__link menu__link--footer">
+                    <IconNews />
+                    <span>Noticias</span>
+                </Link
             >
         </li>
         <li class="menu__item menu__item--footer">
             <Link
                 href="/#contact"
                 native={true}
-                className="menu__link menu__link--footer">Contacto</Link
+                className="menu__link menu__link--footer">
+                    <IconContact />
+                    <span>Contacto</span>
+                </Link
             >
         </li>
     </menu>
---
+++ frontend/src/layouts/icons/IconStation.svelte
@@ -0,0 +1,28 @@
+<svg
+    fill="#000000"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label="Estación"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier"
+        ><path
+            d="M9 11.73a2 2 0 1 1 2 0V20H9v-8.27zm5.24 2.51l-1.41-1.41A3.99 3.99 0 0 0 10 6a4 4 0 0 0-2.83 6.83l-1.41 1.41a6 6 0 1 1 8.49 0zm2.83 2.83l-1.41-1.41a8 8 0 1 0-11.31 0l-1.42 1.41a10 10 0 1 1 14.14 0z"
+        ></path></g
+    ></svg
+>
+
+<style>
+    svg {
+        min-width: var(--icon-width);
+        min-height: var(--icon-height);
+        max-width: var(--icon-width);
+        max-height: var(--icon-height);
+    }
+
+    path {
+        fill: var(--icon-color);
+    }
+</style>
