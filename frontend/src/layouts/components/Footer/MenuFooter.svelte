481d2c3b8391d92565b21c15295be5ae132207de
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-10-12 12:18:25 -0500

    update

--- frontend/src/layouts/components/Footer/MenuFooter.svelte
+++ frontend/src/layouts/components/Footer/MenuFooter.svelte
@@ -2,6 +2,7 @@
     import IconContact from "../../icons/IconContact.svelte";
     import IconHome from "../../icons/IconHome.svelte";
     import IconLive from "../../icons/IconLive.svelte";
+    import IconMenuItem from "../../icons/IconMenuItem.svelte";
     import IconNews from "../../icons/IconNews.svelte";
     import IconStation from "../../icons/IconStation.svelte";
     import Link from "../../routers/components/Link.svelte";
@@ -9,7 +10,8 @@
 
 <div class="grow">
     <h3 class="footer__title footer__title--menu" aria-label="Navegación">
-        Navegación
+        <IconMenuItem />
+        <span>Navegación</span>
     </h3>
     <menu class="menu menu--footer">
         <li class="menu__item menu__item--footer">
@@ -27,43 +29,43 @@
             <Link
                 href="/#programming"
                 native={true}
-                className="menu__link menu__link--footer">
-                    <IconStation />
-                    <span>Programación</span>
-                </Link
+                className="menu__link menu__link--footer"
             >
+                <IconStation />
+                <span>Programación</span>
+            </Link>
         </li>
 
         <li class="menu__item menu__item--footer">
             <Link
                 href="/online"
                 native={false}
-                className="menu__link menu__link--footer">
-                    <IconLive />
-                    <span>En Vivo</span>
-                </Link
+                className="menu__link menu__link--footer"
             >
+                <IconLive />
+                <span>En Vivo</span>
+            </Link>
         </li>
 
         <li class="menu__item menu__item--footer">
             <Link
                 href="/news"
                 native={false}
-                className="menu__link menu__link--footer">
-                    <IconNews />
-                    <span>Noticias</span>
-                </Link
+                className="menu__link menu__link--footer"
             >
+                <IconNews />
+                <span>Noticias</span>
+            </Link>
         </li>
         <li class="menu__item menu__item--footer">
             <Link
                 href="/#contact"
                 native={true}
-                className="menu__link menu__link--footer">
-                    <IconContact />
-                    <span>Contacto</span>
-                </Link
+                className="menu__link menu__link--footer"
             >
+                <IconContact />
+                <span>Contacto</span>
+            </Link>
         </li>
     </menu>
 </div>
