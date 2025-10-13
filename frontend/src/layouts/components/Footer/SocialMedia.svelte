f17b7c66c83f51ca17f15d712b09ef5f8d21a367
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-10-13 12:29:52 -0500

    Update

--- frontend/src/layouts/components/Footer/SocialMedia.svelte
+++ frontend/src/layouts/components/Footer/SocialMedia.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+    import IconFacebook from "../../icons/IconFacebook.svelte";
+    import IconInstagram from "../../icons/IconInstagram.svelte";
+    import IconPatreon from "../../icons/IconPatreon.svelte";
+    import IconPinterest from "../../icons/IconPinterest.svelte";
     import IconSocialMedia from "../../icons/IconSocialMedia.svelte";
+    import IconThreads from "../../icons/IconThreads.svelte";
+    import IconTikTok from "../../icons/IconTikTok.svelte";
+    import IconX from "../../icons/IconX.svelte";
+    import IconYouTube from "../../icons/IconYouTube.svelte";
 </script>
 
 <div class="links" id="links">
@@ -9,6 +17,84 @@
     </h3>
 
     <ul class="social social--footer">
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="Pinterest"
+            >
+                <IconPinterest />
+            </a>
+        </li>
+
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="X"
+            >
+                <IconX />
+            </a>
+        </li>
+
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="Facebook"
+            >
+                <IconFacebook />
+            </a>
+        </li>
+
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="TikTok"
+            >
+                <IconTikTok />
+            </a>
+        </li>
         
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="Instagram"
+            >
+                <IconInstagram />
+            </a>
+        </li>
+        
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="YouTube"
+            >
+                <IconYouTube />
+            </a>
+        </li>
+
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="Threads"
+            >
+                <IconThreads />
+            </a>
+        </li>
+
+        <li class="social__item social__item--footer">
+            <a
+                href="#home"
+                class="social__link social__link--footer"
+                aria-label="Patreon"
+            >
+                <IconPatreon />
+            </a>
+        </li>
     </ul>
 </div>
---
+++ frontend/src/layouts/icons/IconYouTube.svelte
@@ -0,0 +1,32 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    style="width:100%;height:100%"
+    class="external-icon"
+    display="block"
+    pointer-events="none"
+    viewBox="0 0 28.57 20"
+    aria-label="YouTube"
+    ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28.57 20"
+        ><path
+            class="path"
+            fill="red"
+            d="M27.973 3.123A3.578 3.578 0 0025.447.597C23.22 0 14.285 0 14.285 0S5.35 0 3.123.597A3.578 3.578 0 00.597 3.123C0 5.35 0 10 0 10s0 4.65.597 6.877a3.578 3.578 0 002.526 2.526C5.35 20 14.285 20 14.285 20s8.935 0 11.162-.597a3.578 3.578 0 002.526-2.526C28.57 14.65 28.57 10 28.57 10s-.002-4.65-.597-6.877z"
+        /><path
+            fill="#fff"
+            d="M11.425 14.285L18.848 10l-7.423-4.285v8.57z"
+        /></svg
+    ></svg
+>
+
+<style>
+    svg {
+        min-width: var(--icon-width);
+        min-height: var(--icon-height);
+        max-width: var(--icon-width);
+        max-height: var(--icon-height);
+    }
+
+    .path {
+        fill: var(--icon-color);
+    }
+</style>
