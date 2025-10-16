f260d60b87f42153c786308428f62fd152842d69
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2025-10-16 07:29:24 -0500

    Update

--- frontend/src/layouts/components/Footer/InfoContact.svelte
+++ frontend/src/layouts/components/Footer/InfoContact.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import IconAddress from "../../icons/IconAddress.svelte";
     import IconContact from "../../icons/IconContact.svelte";
+    import IconEmail from "../../icons/IconEmail.svelte";
+    import IconWhatsApp from "../../icons/IconWhatsApp.svelte";
 </script>
 
 <div class="informations" id="links">
@@ -13,6 +15,16 @@
         <li class="info__item info__item--contact">
             <IconAddress />
             <span>Alguna dirección, calle falsa</span>
+        </li>
+        
+        <li class="info__item info__item--contact">
+            <IconEmail />
+            <span>correo@dominio.com</span>
+        </li>
+        
+        <li class="info__item info__item--contact">
+            <IconWhatsApp />
+            <span>+1 000 000 0000</span>
         </li>
     </ul>
 </div>
